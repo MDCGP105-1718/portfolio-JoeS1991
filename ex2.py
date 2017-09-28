f8c11d6df8d1fa82e3b947f5691d0373997146f4
Joe Shuttlewood <joeshuttlewood@hotmail.co.uk>
2017-09-28 13:15:59 +0100

    First week .py files from the first steps with python .pdf, code commented with my thought processes and some extra test lines.

---
+++ ex2.py
@@ -0,0 +1,21 @@
+print("These are how many animals I have:")
+
+print("Cows", 15 + 30 / 6) #the '+' sign is addition, the '/' sign is division.
+print("Sheep", 100 - 30 * 3 % 4) #the '-' sign is subtraction, the '*' sign is multiplication, the '%' sign is ???.
+#I've looked up that % gives the remainder of a division.
+print("Now I will count my plants:")
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6) #do I need space between these?
+print("Extra testing line, see code comment", 3+2+1-5+4%2-1/4+6) #testing if spaces are required. They are not.
+
+print("Is it true that 3 + 2 < 5 - 7?") #no arithmatic is taking place in this line, as it is in quotes.
+print(3 + 2 < 5 - 7) #This line simplifies to 5 < -2 - which is False, so it prints False.
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Can you see why that printed False earlier?") #yes, commented on line 12.
+
+print("Is it greater?", 5 < -2)
+print("Is it greater of equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2) #Lines 19, 20, 21 - the '<' sign is less than, '>' is greater than, similarly '<=' and '>=' are less than/greater than or equal to.
