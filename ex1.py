bee756b524f3c81201a80604f11e716adf42b2ec
JoeS1991 <joeshuttlewood@hotmail.co.uk>
2017-10-05 11:04:29 +0100

    This commit is to remove ex1/2/3/4 from the top level folder for the sake of organisation. Hopefully I don't deleted them from the Week 1 folder too...

--- ex1.py
+++
@@ -1,6 +0,0 @@
-print("Hello World!")
-print('This is exciting.')
-print("Isn't this a thrill?")
-print("I said 'isn't this a thrill?''")
-print('No, you are right, it is "boring".')
-print("How about this?") #trying to get a syntax error by using double/single quotes
