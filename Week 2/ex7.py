02dc00e92aa32a2cbf435c54c270ed3e392fa595
Joe Shuttlewood <joeshuttlewood@hotmail.co.uk>
2017-10-06 11:15:00 +0100

    I noticed some fundamental errors with ex7.py, trying to correct them, this is my first effort to do so.

--- Week 2/ex7.py
+++ Week 2/ex7.py
@@ -1,16 +1,17 @@
-total_cost = float(input("How much does your dream home cost, in GBP? "))
-portion_deposit = total_cost * 0.2
 current_savings = 0
-annual_salary = float(input("How much do you earn per year, in GBP? "))
+annual_salary = float(input("Enter your annual salary: "))
 monthly_salary = annual_salary/12
-portion_saved = float(input("As a decimal (i.e. 0.1 is 10%, 0.45 is 45%), how much of your income to you put in to savings? "))
+portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
+total_cost = float(input("Enter the cost of your dream home: "))
+portion_deposit = total_cost * 0.2
 r = 0.04
 roi = current_savings*r/12 #I have decided to make roi its own variable so I can just add this instead of putting current_savings*r/12 everywhere I want to calculate it.
 monthly_savings = (monthly_salary * portion_saved) + roi #It took me 20 minutes to realise I'd made an error on this line and was dividing monthly_salary by portion_saved...
 months = 0
 
-while current_savings < total_cost:
+while current_savings < portion_deposit:
     months +=1
-    current_savings = current_savings + monthly_savings
+    current_savings += monthly_savings #There's something wrong with my maths here. Maybe I need to split it to two lines?
+    current_savings += roi #Nope, still something fundamentally wrong with how I'm calculating this. Going to commit anyway to display my progress.
 
-print(f"It will take you {months} months to save for your dream home.")
+print(f"Number of months: {months}")
