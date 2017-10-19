9ed4dd01428a1f2154645a72cb081659cf1dc68c
JoeS1991 <joeshuttlewood@hotmail.co.uk>
2017-10-19 11:04:21 +0100

    Added ex11.py.

---
+++ Week 4/ex11.py
@@ -0,0 +1,22 @@
+from random import randint
+
+guess = int(input("Guess the number: "))
+guesses = 0
+num = randint(1,100)
+
+def guessCheck(x):
+    if x == num:
+        print(f"Correct! The number was {num}.")
+    elif x > num:
+        print("Lower.")
+    elif x < num:
+        print("Higher.")
+
+while guess != num:
+    if guess != num:
+        guessCheck(guess)
+        guesses += 1
+        guess = int(input("Try again: "))
+
+if guess == num:
+    print(f"Correct! The number was {num} and it took you {guesses} guesses.")
